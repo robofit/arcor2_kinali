6200dad3725d483667e8b70a748489b17f9973cf
Michal Kapinus <michal.kapinus@gmail.com>
2019-08-08 12:42:14 +0200

    typo

--- arcor2_kinali/arcor2_kinali/conf.py
+++ arcor2_kinali/arcor2_kinali/conf.py
@@ -1,9 +1,9 @@
 import swagger_client
 
 try:
-    KINALI_RESTAPI_ADRESS = os.environ["ARCOR2_KINALI_RESTAPI_ADRESS"]
+    KINALI_RESTAPI_ADDRESS = os.environ["ARCOR2_KINALI_RESTAPI_ADDRESS"]
 except KeyError:
-    sys.exit("'ARCOR2_KINALI_RESTAPI_ADRESS' env. variable not set.")
+    sys.exit("'ARCOR2_KINALI_RESTAPI_ADDRESS' env. variable not set.")
 
 API_CLIENT_CONF = swagger_client.Configuration()
-API_CLIENT_CONF.host = KINALI_RESTAPI_ADRESS
+API_CLIENT_CONF.host = KINALI_RESTAPI_ADDRESS

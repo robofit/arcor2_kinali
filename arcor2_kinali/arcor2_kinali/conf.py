aca14b59d70d4a14ced1c3006c27d9c4a41193d3
Michal Kapinus <michal.kapinus@gmail.com>
2019-08-08 12:39:34 +0200

    kinali restapi address as an ENV variable

--- arcor2_kinali/arcor2_kinali/conf.py
+++ arcor2_kinali/arcor2_kinali/conf.py
@@ -1,4 +1,9 @@
 import swagger_client
 
+try:
+    KINALI_RESTAPI_ADRESS = os.environ["ARCOR2_KINALI_RESTAPI_ADRESS"]
+except KeyError:
+    sys.exit("'ARCOR2_KINALI_RESTAPI_ADRESS' env. variable not set.")
+
 API_CLIENT_CONF = swagger_client.Configuration()
-API_CLIENT_CONF.host = "http://localhost:5000"
+API_CLIENT_CONF.host = KINALI_RESTAPI_ADRESS

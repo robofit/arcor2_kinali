409fd3701bfdc62cd090fb68dde207dd2500e017
ZdenekM <imaterna@fit.vutbr.cz>
2020-01-23 14:59:05 +0100

    Proper except.

--- arcor2_kinali/object_types/rest_robot.py
+++ arcor2_kinali/object_types/rest_robot.py
@@ -6,7 +6,7 @@ from arcor2.data.object_type import MeshFocusAction, Models
 try:
     # for development
     from arcor2_kinali.services.rest_robot_service import RestRobotService, MoveTypeEnum
-except ImportError:
+except (ImportError, ModuleNotFoundError):
     # for execution package
     from ..services.rest_robot_service import RestRobotService, MoveTypeEnum
 from arcor2.action import action

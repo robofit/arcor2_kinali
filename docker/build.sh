c79d3eaf5138dc8e1178446e4463fec0424e64e1
Michal Kapinus <michal.kapinus@gmail.com>
2020-03-30 10:15:09 +0200

    build script updated

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
+BASE_VERSION=latest
 VERSION=latest
+docker build --no-cache -f Dockerfile -t arcor2/arcor2_base_kinali:$VERSION  ../ --build-arg version=$BASE_VERSION
 docker build --no-cache -f Dockerfile-arserver -t arcor2/arcor2_arserver_kinali:$VERSION  ../ --build-arg version=$VERSION
 docker build --no-cache -f Dockerfile-execution -t arcor2/arcor2_execution_kinali:$VERSION --build-arg version=$VERSION ../   

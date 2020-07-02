ee78fb750775968dcf3b5990859fbddebbd07725
ZdenekM <imaterna@fit.vutbr.cz>
2020-07-02 11:56:02 +0200

    arcor2 version update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         'dataclasses',
         'dataclasses-jsonschema[fast-validation]',
-        'arcor2==0.8.0b2',
+        'arcor2==0.8.0b3',
         'Pillow'
     ],
     zip_safe=False,
